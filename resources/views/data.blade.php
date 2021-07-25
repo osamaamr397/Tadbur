fa14b2f04ddd80c31c6655c436b1640960803d29
osamaamr397 <47090033+osamaamr397@users.noreply.github.com>
2021-07-25 17:13:27 +0200

    fourth commit

---
+++ resources/views/data.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet"href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+    <title></title>
+
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
